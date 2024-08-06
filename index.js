2fd2d69ce4a92e657a5d646096ad7f48ac23ff6d
carlos bernal <bernal199152@yahoo.com>
2024-08-06 15:27:28 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    let a= 2000
+    let b=100
+ return a/b;
+}
+
+let square = (n) => n * n;
+let x = 2
+console.log(square(x));
+
+const add = (a,b) => a+b
+let a = 3
+let b = 4
+console.log(a,b);
